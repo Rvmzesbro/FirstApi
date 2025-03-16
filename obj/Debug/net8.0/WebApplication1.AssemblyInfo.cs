0c1bd6a340a2d2bcedf5735ce0f0816a44f16afa
Rvmzesbro <galeevramazan64@gmail.com>
2025-03-16 12:59:22 +0300

    second

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98f45dda3cd4859c44f2b10ac9939662f2da627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8464e9b5130e4e5c11dc89f5bbdcb5a8d72af59a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
